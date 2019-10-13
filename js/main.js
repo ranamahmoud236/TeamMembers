08cb3ad0f2cef50989c400b021f8e801bbaf5220
ranamahmoud236 <ranaimteer@gmail.com>
2019-10-13 23:26:05 +0300

    7

---
+++ js/main.js
@@ -0,0 +1,16 @@
+class Members {
+    constructor(Name, Email, Major, Role, Biograph)
+    {
+        this.name = name ;
+        this.email=email;
+        this.major=major;
+        this.role=role;
+        this.Biograph=Biograph;
+        this.index=index;
+      }
+      function AddMembers(members) {
+
+        
+      }
+
+}
