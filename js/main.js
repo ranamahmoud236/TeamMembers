3faaed1f1ccd06e71060bd703eea6820df3860e5
ranamahmoud236 <ranaimteer@gmail.com>
2019-10-14 23:32:19 +0300

    10

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 class Member{
-  constructor(Name, Email, Major, Role, Biograph)
+  constructor(Name, Email, Major, Role, biograph,index)
     {
         this.name = name ;
         this.email=email;
         this.major=major;
         this.role=role;
-        this.Biograph=Biograph;
+        this.biograph=biograph;
         this.index=index ;
     }
 }
@@ -13,6 +13,8 @@ class Members {
   constructor(members){
     localStorage.getItem('members');
     this.members = JSON.parse(localStorage.getItem('members')) ;
+    members=document.getElementById('members');
+  
         const members= [] ;
         if(members==null){
           return this.members=null ;
@@ -23,12 +25,32 @@ class Members {
       return this.members ;
     }
     function addMember() {
+      let member = JSON.parse(localStorage.getElementById('member'));
+      let memberList = document.getElementById('memberList');
+      memberList.innerHTML = '';
       for(let i=0;i++;i<1000){
-        members[i]=members ;
+        let name = members[i].name ;
+        let email = members[i].email;
+        let major = members[i].major;
+        let role = members[i].role;
+        let biograph = members[i].biograph ;
+        let index =members[i].index ;
+
+        memberList.innerHTML += ''
 
       }
     }
+    function deleteMember(member) {
+      let member = JSON.parse(localStorage.getElementById('member'));
+      for(let i=0 ; i<member.length;i++){
+        if (member[i]=member){
+          member.splice(i,1);
+        }
+      }
+      localStorage.setItem('member',JSON.stringify(member));
+      addMember() ;
+    }
     
-  
+
     
 
