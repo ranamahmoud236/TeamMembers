77ab22a073e65d0057215abd53827424dba0c9f4
ranamahmoud236 <ranaimteer@gmail.com>
2019-10-14 20:57:25 +0300

    9

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-class Members {
-    constructor(Name, Email, Major, Role, Biograph)
+class Member{
+  constructor(Name, Email, Major, Role, Biograph)
     {
         this.name = name ;
         this.email=email;
         this.major=major;
         this.role=role;
         this.Biograph=Biograph;
-        this.index=index;
-      }
-      function AddMembers(members) {
-
-        
-      }
-
+        this.index=index ;
+    }
 }
+class Members {
+  constructor(members){
+    localStorage.getItem('members');
+    this.members = JSON.parse(localStorage.getItem('members')) ;
+        const members= [] ;
+        if(members==null){
+          return this.members=null ;
+        }
+      }
+    }
+    function getMember(members) {
+      return this.members ;
+    }
+    function addMember() {
+      for(let i=0;i++;i<1000){
+        members[i]=members ;
+
+      }
+    }
+    
+  
+    
+
